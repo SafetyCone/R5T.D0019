e679bec6dbf33d61f9760a296e65552c1c244497
MinexAutomation <minex.automation@gmail.com>
2020-06-22 00:00:52 -0700

    Added R5T.D0019.Default (VisualStudioProjectFileTransformer implementation of IVisualStudioProjectFileTransformer) project. Fixed namespace to be R5T.D0019.

--- source/R5T.D0019.Base/Code/Services/Definitions/IVisualStudioProjectFileTransformer.cs
+++ source/R5T.D0019.Base/Code/Services/Definitions/IVisualStudioProjectFileTransformer.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using R5T.T0002;
 
 
-namespace R5T.T0004.Construction
+namespace R5T.D0019
 {
     /// <summary>
     /// Working at the <see cref="IVisualStudioProjectFile"/>-level of abstraction, transforms one instance to be equal to the other instance.
---
+++ source/R5T.D0019.Default/Code/Services/Implementations/VisualStudioProjectFileTransformer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.T0002;
+
+
+namespace R5T.D0019.Default
+{
+    public class VisualStudioProjectFileTransformer : IVisualStudioProjectFileTransformer
+    {
+        public Task CopySourceToDestinationAsync(IVisualStudioProjectFile source, IVisualStudioProjectFile destination)
+        {
+            destination.GenerateDocumentationFile = source.GenerateDocumentationFile;
+            destination.IsPackable = source.IsPackable;
+            destination.LanguageVersion = source.LanguageVersion;
+            destination.NoWarn = source.NoWarn;
+            destination.OutputType = source.OutputType;
+            destination.SDK = source.SDK;
+            destination.TargetFramework = source.TargetFramework;
+
+            destination.ClonePackageReferencesFrom(source);
+            destination.CloneProjectReferencesFrom(source);
+
+            return Task.CompletedTask;
+        }
+    }
+}
