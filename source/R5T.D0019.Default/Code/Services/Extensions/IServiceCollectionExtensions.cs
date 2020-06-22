c6fa491046e6018a703b56a4161c85187ec6c343
MinexAutomation <minex.automation@gmail.com>
2020-06-22 00:05:44 -0700

    Added R5T.Dacia.Extensions (DI extensions) project-reference dependency. Added IServiceCollection extensions.

---
+++ source/R5T.D0019.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.D0019.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="VisualStudioProjectFileTransformer"/> implementation of <see cref="IVisualStudioProjectFileTransformer"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddVisualStudioProjectFileTransformer(this IServiceCollection services)
+        {
+            services.AddSingleton<IVisualStudioProjectFileTransformer, VisualStudioProjectFileTransformer>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="VisualStudioProjectFileTransformer"/> implementation of <see cref="IVisualStudioProjectFileTransformer"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<IVisualStudioProjectFileTransformer> AddVisualStudioProjectFileTransformerAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction<IVisualStudioProjectFileTransformer>.New(() => services.AddVisualStudioProjectFileTransformer());
+            return serviceAction;
+        }
+    }
+}
